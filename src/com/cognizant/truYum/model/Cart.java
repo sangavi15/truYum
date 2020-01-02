5712d71b492f215e7318ee80038d8c29586df3df
Karnan <835196@cognizant.com>
2020-01-02 14:55:14 +0530

    java

---
+++ src/com/cognizant/truYum/model/Cart.java
@@ -0,0 +1,33 @@
+package com.cognizant.truYum.model;
+
+import java.util.List;
+
+public class Cart {
+    private List<MenuItem> menuItemList;
+    private double total;
+     private Cart() {
+
+}
+    public Cart(List<MenuItem> menuItemList, double total) {
+        super();
+        this.menuItemList = menuItemList;
+        this.total = total;
+    }
+    public List<MenuItem> getMenuItemList() {
+        return menuItemList;
+    }
+    public void setMenuItemList(List<MenuItem> menuItemList) {
+        this.menuItemList = menuItemList;
+    }
+    public double getTotal() {
+        return total;
+    }
+    public void setTotal(double total) {
+        this.total = total;
+    }
+    @Override
+    public String toString() {
+        return "Cart [menuItemList=" + menuItemList + ", total=" + total + "]";
+    }
+     
+}
