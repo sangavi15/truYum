1953a750278ae7542220dc050bc29daa780d3968
Karnan <835196@cognizant.com>
2020-01-16 18:32:33 +0530

    Servlet&JSP

--- src/com/cognizant/truYum/servlet/AddToCartServlet.java
+++ src/com/cognizant/truYum/servlet/AddToCartServlet.java
@@ -21,6 +21,7 @@ public class AddToCartServlet extends HttpServlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+
         long menuItemId = Long.parseLong(request.getParameter("menuItemId"));
         long userId = 1L;
         CartDao cartDao = new CartDaoCollectionImpl();
