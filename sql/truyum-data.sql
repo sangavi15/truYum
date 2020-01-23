414f3f00695e99a40cc96143092fe613b58a8ba3
Karnan <835196@cognizant.com>
2020-01-23 17:58:18 +0530

    sql

---
+++ sql/truyum-data.sql
@@ -0,0 +1,91 @@
+-- 1. View Menu Item List Admin(TYUC001)
+-- a. Inserting menu items
+
+insert into truyum.menu_item(me_name,me_price,me_active,me_date_of_launch,me_category,me_free_delivery) values('Sandwich', 99.00,'Yes','2017-03-15','Main Course','Yes');
+insert into truyum.menu_item(me_name,me_price,me_active,me_date_of_launch,me_category,me_free_delivery) values('Burger', 129.00,'Yes','2017-12-23','Main Course','No');
+insert into truyum.menu_item(me_name,me_price,me_active,me_date_of_launch,me_category,me_free_delivery) values('Pizza', 149.00,'Yes','2018-08-21','Main Course','No');
+insert into truyum.menu_item(me_name,me_price,me_active,me_date_of_launch,me_category,me_free_delivery) values('French Fries', 57.00,'No','2017-07-02','Starters','Yes');
+insert into truyum.menu_item(me_name,me_price,me_active,me_date_of_launch,me_category,me_free_delivery) values('Chocolate', 32.00,'Yes','2022-11-02','Dessert','Yes');
+
+-- b. Retrieving menu items(admin view)
+SELECT 
+    *
+FROM
+    truyum.menu_item;
+-- 2. View Menu Item List Customer(TYUC002)
+-- a. Retrieving menu items(customer view)
+SELECT 
+    *
+FROM
+    truyum.menu_item
+WHERE
+    me_active = 'Yes'
+        AND me_date_of_launch < (SELECT CURDATE());
+
+-- 3. Edit Menu Item(TYUC003)
+-- a. Retrieving particular menu item for edit
+SELECT 
+    *
+FROM
+    truyum.menu_item
+WHERE
+    me_id = 1;
+-- b. Uploading menu item id=1
+UPDATE truyum.menu_item 
+SET 
+    me_name = 'Cheesy Sandwich',
+    me_price = 150.00,
+    me_active = 'No',
+    me_date_of_launch = '2018-03-15',
+    me_category = 'Status',
+    me_free_delivery = 'No'
+WHERE
+    me_id = 1;
+
+-- 4. Add to Cart(TYUC004)
+-- a. Inserting user details and adding cart information in their user id
+
+insert into truyum.user(us_name)values('Hari');
+insert into truyum.user(us_name)values('Vicky');
+
+
+
+insert into truyum.cart(ct_us_id,ct_pr_id)values(1,1);
+insert into truyum.cart(ct_us_id,ct_pr_id)values(1,1);
+insert into truyum.cart(ct_us_id,ct_pr_id)values(1,3);
+
+-- 5. View Cart(TYUC005)
+-- a. Retrieving all the carts from the particular user id
+SELECT 
+    me.me_id,
+    me.me_name,
+    me.me_price,
+    me.me_active,
+    me.me_date_of_launch,
+    me.me_category,
+    me.me_free_delivery
+FROM
+    truyum.menu_item me
+        JOIN
+    truyum.cart ct ON me.me_id = ct.ct_pr_id
+        JOIN
+    truyum.user us ON us.us_id = ct.ct_us_id
+WHERE
+    us.us_id = 1;
+
+-- b. Total amount
+SELECT 
+    SUM(me.me_price) AS Total
+FROM
+    truyum.cart ct
+        JOIN
+    truyum.menu_item me ON ct.ct_pr_id = me.me_id
+        JOIN
+    truyum.user us ON us.us_id = ct.ct_us_id
+WHERE
+    us.us_id = 1;
+
+-- 6. Remove Item from Cart('TYUC006)
+-- a. Removing item from the cart 
+
+delete from truyum.cart where ct_us_id=1 and ct_pr_id=1 limit 1;
---
+++ sql/truyum-schema-creation.sql
@@ -0,0 +1,106 @@
+-- MySQL dump 10.13  Distrib 8.0.11, for Win64 (x86_64)
+--
+-- Host: localhost    Database: truyum
+-- ------------------------------------------------------
+-- Server version	8.0.11
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+ SET NAMES utf8 ;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `cart`
+--
+
+DROP TABLE IF EXISTS `cart`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `cart` (
+  `ct_id` int(11) NOT NULL AUTO_INCREMENT,
+  `ct_us_id` int(11) DEFAULT NULL,
+  `ct_pr_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`ct_id`),
+  KEY `ct_us_id` (`ct_us_id`),
+  KEY `ct_pr_id` (`ct_pr_id`),
+  CONSTRAINT `cart_ibfk_1` FOREIGN KEY (`ct_us_id`) REFERENCES `user` (`us_id`),
+  CONSTRAINT `cart_ibfk_2` FOREIGN KEY (`ct_pr_id`) REFERENCES `menu_item` (`me_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cart`
+--
+
+LOCK TABLES `cart` WRITE;
+/*!40000 ALTER TABLE `cart` DISABLE KEYS */;
+/*!40000 ALTER TABLE `cart` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `menu_item`
+--
+
+DROP TABLE IF EXISTS `menu_item`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `menu_item` (
+  `me_id` int(11) NOT NULL AUTO_INCREMENT,
+  `me_name` varchar(100) DEFAULT NULL,
+  `me_price` decimal(8,2) DEFAULT NULL,
+  `me_active` varchar(3) DEFAULT NULL,
+  `me_date_of_launch` date DEFAULT NULL,
+  `me_category` varchar(45) DEFAULT NULL,
+  `me_free_delivery` varchar(3) DEFAULT NULL,
+  PRIMARY KEY (`me_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `menu_item`
+--
+
+LOCK TABLES `menu_item` WRITE;
+/*!40000 ALTER TABLE `menu_item` DISABLE KEYS */;
+/*!40000 ALTER TABLE `menu_item` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `user` (
+  `us_id` int(11) NOT NULL AUTO_INCREMENT,
+  `us_name` varchar(60) DEFAULT NULL,
+  PRIMARY KEY (`us_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-01-23 15:59:23
